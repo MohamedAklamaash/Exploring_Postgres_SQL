03463350203c2249ad734ae14bd8f61a6ea03684
MohamedAklamaash <aklamaash78@gmail.com>
2023-12-04 21:12:59 +0530

    login functionality issue

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,26 +1,22 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import { googleLogout } from "@react-oauth/google";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const LoginPage = ({ setUserData, userData }) => {
-  useEffect(()=>{
-    console.log(userData);
-  },[userData])
+  const [data, setdata] = useState({});
   return (
     <div className=" flex items-center gap-6 ">
       <GoogleLogin
         onSuccess={async(credentialResponse) => {
           const decoded = jwtDecode(credentialResponse.credential);
-          console.log(decoded);
-          await setUserData(decoded);
-          console.log(userData);
+          setdata(decoded);
+          console.log(data);
           window.location = "/";
         }}
         onError={() => {
           console.log("Login Failed ");
         }}
-        useOneTap
       />
       <button
         onClick={() => {
